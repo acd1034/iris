ecb8cbcbf20c8e3a474f62adc687f266f6d0b8d2
acd1034 <48613285+acd1034@users.noreply.github.com>
2019-12-09 18:30:33 +0900

    add type detector of range

--- include/iris/ranges.hpp
+++ include/iris/ranges.hpp
@@ -4,11 +4,28 @@
 namespace iris {
   namespace ranges {
     template <typename R>
-    using class_begin_t = decltype(std::declval<R>().begin());
+    using std_begin_t = decltype(std::begin(std::declval<R>()));
     template <typename R>
-    using class_end_t = decltype(std::declval<R>().end());
+    using std_end_t = decltype(std::end(std::declval<R>()));
+    template <typename R>
+    using adl_begin_t = decltype(begin(std::declval<R>()));
+    template <typename R>
+    using adl_end_t = decltype(end(std::declval<R>()));
   } // namespace ranges
-
+  template <typename R>
+  using iterator_t =
+    detected_or_t<detected_t<ranges::std_begin_t, R&>, ranges::adl_begin_t, R&>;
+  template <typename R>
+  using sentinel_t =
+    detected_or_t<detected_t<ranges::std_end_t, R&>, ranges::adl_end_t, R&>;
+  template <typename R>
+  using range_value_t = iter_value_t<iterator_t<R>>;
+  template <typename R>
+  using range_reference_t = iter_reference_t<iterator_t<R>>;
+  // template <typename R>
+  // using range_rvalue_reference_t = iter_rvalue_reference_t<iterator_t<R>>;
+  // template <typename R>
+  // using range_difference_t = iter_difference_t<iterator_t<R>>;
 #define IRIS_DEFINE_UNARY_CONCEPT(Name, Type, ...)                             \
   template <typename Type>                                                     \
   using Name = std::conjunction<__VA_ARGS__>;                                  \
