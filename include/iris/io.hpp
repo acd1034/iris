5dcdab859c60b9a1c75ae74a1d6d9981d32dbca3
acd1034 <48613285+acd1034@users.noreply.github.com>
2020-02-06 15:15:14 +0900

    is_stream_insertable

--- include/iris/io.hpp
+++ include/iris/io.hpp
@@ -4,26 +4,42 @@
 #include <iris/utility/is_tuple_like.hpp> // is_tuple_like
 
 namespace iris {
+  namespace io_impl {
+    template <typename T>
+    struct is_std_basic_ostream : std::false_type {};
+    template <typename CharT, typename Traits>
+    struct is_std_basic_ostream<std::basic_ostream<CharT, Traits>> : std::true_type {};
+    template <typename T>
+    inline constexpr bool is_std_basic_ostream_v = is_std_basic_ostream<T>::value;
+
+    template <typename CharT, typename Traits, typename T, std::size_t... Indicies>
+    auto& tuple_io_impl(std::basic_ostream<CharT, Traits>& os, const T& t,
+                        std::index_sequence<Indicies...>) {
+      const char* dlm = "";
+      using swallow   = std::initializer_list<int>;
+      (void)swallow{
+        (void(os << std::exchange(dlm, " ") << get<Indicies>(t)), 0)...};
+      return os;
+    }
+  } // namespace io_impl
+  template <typename Os, typename T>
+  using is_stream_insertable = std::conjunction<
+    io_impl::is_std_basic_ostream<Os>,
+    is_detected_exact<Os&, concepts::bit_left_shift_t, Os&, const T&>>;
+  template <typename Os, typename T>
+  inline constexpr bool is_stream_insertable_v = is_stream_insertable<Os, T>::value;
+
   inline namespace io {
     inline namespace ranges_io {
-      // range operator<<
-      template <
-        typename CharT,
-        typename Traits,
-        typename R,
-        enable_if_t<
-          is_range_v<R> && !is_range_v<range_value_t<R>>> = nullptr>
+      template <typename CharT, typename Traits, typename R,
+                enable_if_t<is_range_v<R> && !is_range_v<range_value_t<R>>> = nullptr>
       auto& operator<<(std::basic_ostream<CharT, Traits>& os, const R& r) {
         const char* dlm = "";
         for (const auto& x : r) os << std::exchange(dlm, " ") << x;
         return os;
       }
-      template <
-        typename CharT,
-        typename Traits,
-        typename R,
-        enable_if_t<
-          is_range_v<R> && is_range_v<range_value_t<R>>> = nullptr>
+      template <typename CharT, typename Traits, typename R,
+                enable_if_t<is_range_v<R> && is_range_v<range_value_t<R>>> = nullptr>
       auto& operator<<(std::basic_ostream<CharT, Traits>& os, const R& r) {
         const char* dlm = "";
         for (const auto& x : r) os << std::exchange(dlm, "\n") << x;
@@ -33,26 +49,10 @@ namespace iris {
 
     inline namespace tuple_io {
       using ::iris::ranges_io::operator<<;
-      // tuple-like operator<<
-      namespace _tuple_print {
-        template <typename CharT, typename Traits, typename T, std::size_t... Indicies>
-        auto& tuple_print(std::basic_ostream<CharT, Traits>& os,
-                          const T& t,
-                          std::index_sequence<Indicies...>) {
-          const char* dlm = "";
-          using swallow   = std::initializer_list<int>;
-          (void)swallow{
-            (void(os << std::exchange(dlm, " ") << get<Indicies>(t)), 0)...};
-          return os;
-        }
-      } // namespace _tuple_print
-      template <
-        typename CharT,
-        typename Traits,
-        typename T,
-        enable_if_t<is_tuple_like_v<T> && !is_range_v<T>> = nullptr>
+      template <typename CharT, typename Traits, typename T,
+                enable_if_t<is_tuple_like_v<T> && !is_range_v<T>> = nullptr>
       auto& operator<<(std::basic_ostream<CharT, Traits>& os, const T& t) {
-        return _tuple_print::tuple_print(os, t, std::make_index_sequence<std::tuple_size_v<T>>{});
+        return io_impl::tuple_io_impl(os, t, std::make_index_sequence<std::tuple_size_v<T>>{});
       }
     } // namespace tuple_io
 
